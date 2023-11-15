6240e0d01f4b2caa60be3f592d9bc962a11fa934
ktlhape <ktlhape@iie.ac.za>
2023-11-15 09:04:49 +0200

    Creating project

---
+++ PROG7312-POE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG7312_POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
